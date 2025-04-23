0630771caac829bdaddb9c9c11029540c797a00f
likui628 <90845831+likui628@users.noreply.github.com>
2025-04-23 16:03:09 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <canvas ref="canvasRef"></canvas>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import * as fabric from 'fabric';
+import { ref, onMounted } from 'vue';
+
+const canvasRef = ref<HTMLCanvasElement | null>(null);
+
+onMounted(() => {
+  const canvas = new fabric.Canvas(canvasRef.value, {
+    width: 500,
+    height: 500,
+  });
+  const textValue = 'fabric.js sandbox';
+  const text = new fabric.Textbox(textValue, {
+    originX: 'center',
+    splitByGrapheme: true,
+    width: 200,
+    top: 20,
+    styles: fabric.util.stylesFromArray(
+      [
+        {
+          style: {
+            fontWeight: 'bold',
+            fontSize: 64,
+          },
+          start: 0,
+          end: 9,
+        },
+      ],
+      textValue,
+    ),
+  });
+  canvas.add(text);
+  canvas.centerObjectH(text);
+})
+</script>
